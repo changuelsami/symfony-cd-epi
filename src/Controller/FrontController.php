eb142307807b3d1f761dd89178c15575cb928aef
Sami CHANGUEL <sami.ch.27@gmail.com>
2019-02-13 19:02:12 +0100

    ajout d'un produit au panier

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -5,6 +5,9 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Categorie;
+use App\Entity\Produit;
+
+use Symfony\Component\HttpFoundation\Session\Session;
 
 class FrontController extends Controller
 {
@@ -18,4 +21,39 @@ class FrontController extends Controller
         $liste = $repo->findAll();  
         return $this->render('front/index.html.twig', ['liste' => $liste]);
     }
+
+    /**
+     * @Route("/categorie/{id}", name="la_categorie")
+     */
+    public function categorie(Categorie $categorie)
+    {       
+        $em = $this->getDoctrine()->getManager();
+        $repo = $em->getRepository(Produit::class);
+        $produits = $repo->findByCategorie($categorie);
+        return $this->render('front/produits.html.twig', 
+                            ['cat' => $categorie, 'produits' => $produits]);
+    }
+
+    /**
+     * @Route("/panier/ajouter/{id}", name="panier_ajout")
+     */
+    public function panierAjouter($id) {
+        $sess = new Session();
+
+        $panier = $sess->get('panier');
+        @$panier[$id]++; // $panier[$id] = $panier[$id] + 1;
+        $sess->set('panier', $panier);
+
+        return $this->redirectToRoute('panier_details');
+    }
+
+    /**
+     * @Route("/panier/details", name="panier_details")
+     */
+    public function panierDetails() {
+        
+
+        return $this->render('front/panier.html.twig');
+    }
+
 }
